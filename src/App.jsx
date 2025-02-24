09225f4d30db3a58d86e18afacea0d047d45d939
Pαblo Kuhn <40728995+pa-ku@users.noreply.github.com>
2025-02-23 21:42:17 -0300

    added relative paths and types for paths,

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import Test from '#src/components/test'
+
 
 export default function App() {
   return (
     <>
       <ol className='w-full bg-gradient-to-tl from-sky-800 to-sky-300  h-screen flex flex-wrap items-center gap-5 justify-center '>
-        <Test></Test>
         {[
           'TS',
           'React',
@@ -15,7 +14,7 @@ export default function App() {
           'Lucide',
         ].map((tech) => (
           <p
-            className='text-xl w-34 z-10 shadow-lg text-center shadow-white/30 hover:scale-110 duration-300 bg-white/70  border-2 border-white rounded-xl px-3 py-2 text-sky-700 '
+            className='text-xl w-34 z-10 shadow-lg text-center shadow-white/30 hover:-translate-y-1 duration-300 bg-white/70  border-2 border-white rounded-xl px-3 py-2 text-sky-700 '
             key={tech}
           >
             {tech}
