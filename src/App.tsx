26e633b73ca82642c88bf03dc5e206693ffad23a
Pαblo Kuhn <40728995+pa-ku@users.noreply.github.com>
2025-02-23 23:06:52 -0300

    added a code to copy the repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
+import { Copy } from "lucide-react";
+import { CopyCheck } from "lucide-react";
+import { useState } from "react";
+
 export default function App() {
+  const [copied, setCopied] = useState(false);
+
   return (
     <>
       <main className="flex h-screen w-full flex-col items-center justify-center gap-10 bg-gradient-to-tl from-sky-800 to-sky-300">
         <h1 className="drop-shadow-white text-6xl font-bold text-white">
           VitePlus
         </h1>
+        <p className="flex items-center justify-center gap-2 rounded-lg bg-sky-800 px-3 py-1 text-sky-300">
+          pnpm dlx degit pa-ku/viteplus-template.git app-name
+          <button
+            onClick={() => {
+              navigator.clipboard
+                .writeText(
+                  "pnpm dlx degit pa-ku/viteplus-template.git app-name",
+                )
+                .then(() => {
+                  setCopied(true);
+                  setTimeout(() => {
+                    setCopied(false);
+                  }, 2000);
+                })
+                .catch((err) => {
+                  console.error("Error al copiar al portapapeles: ", err);
+                });
+            }}
+            className="z-10 my-auto cursor-pointer"
+          >
+            {copied ? (
+              <CopyCheck className="size-5" />
+            ) : (
+              <Copy className="size-5" />
+            )}
+          </button>
+        </p>
         <ol className="flex flex-wrap gap-5 drop-shadow-xl">
           {[
             "TS",
